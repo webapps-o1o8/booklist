bdc61a14cacbf6a239ad43f1aff08f08362e7a27
mi-cho <mi-cho@mi-chonoMacBook-Pro.local>
2020-01-03 03:09:35 +0900

    First commit.

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Book;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+Route::get('/', function () {
+    return view('books');
+});
+*/
+Route::group(['middleware' => ['web']], function() {
+
+    Route::get('/', ['middleware' => 'auth',function(){
+        $books = Book::all();
+        return view('books',[
+            'books' => $books
+        ]);
+    }]);
+
+    Route::post('/book', ['middleware' => 'auth' ,function(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $book = new Book; // Eloquant ORM
+        $book->title = $request->name;
+        $book->save();
+
+        return redirect('/');
+    }]);
+
+    Route::delete('/book/{book}', ['middleware' => 'auth' ,function(Book $book) {
+        $book->delete();
+
+        return redirect('/');
+    }]);
+
+    Route::auth();
+});
